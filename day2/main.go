a1d8f4d6708b7a4834acc7f75ae5f1592ff48220
LtdJorge <jorgekq@hotmail.es>
2021-12-02 14:51:23 +0100

    Implementation for d2p1

--- day2/main.go
+++ day2/main.go
@@ -1,6 +1,81 @@
 package main
 
-func main() {
-	var horizontal, depth []int
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+type pair struct {
+	instruction string
+	value int
+}
+
+func main() {
+	var horizontal, depth int = 0, 0
+	forward := "forward"
+	down := "down"
+	up := "up"
+
+	//var pairArray = fileToSlice("test-input.txt")
+	var pairArray = fileToSlice("input.txt")
+	var pairs []pair
+
+	for i := 0; i < len(pairArray); i++ {
+		pair := parsePair(pairArray[i])
+		pairs = append(pairs, pair)
+		switch pair.instruction {
+		case forward:
+			horizontal += pair.value
+		case up:
+			depth -= pair.value
+		case down:
+			depth += pair.value
+		default:
+			log.Fatalf("Unable to parse pair: %s %d", pair.instruction, pair.value)
+		}
+	}
+	fmt.Printf("Horizontal: %d\n", horizontal)
+	fmt.Printf("Depth: %d\n", depth)
+	fmt.Printf("Multiplied: %d\n", horizontal*depth)
+}
+
+func fileToSlice(filename string) []string{
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var points []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		point := scanner.Text()
+		points = append(points, point)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	err = file.Close()
+	if err != nil {
+		return nil
+	}
+	return points
+}
+
+func parsePair(str string) pair {
+	var parts = strings.Split(str, " ")
+	var pair pair
+	pair.instruction = parts[0]
+	var err error
+	pair.value, err = strconv.Atoi(parts[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pair
 }
